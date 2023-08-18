657779cbfd7d25d8054e85546e7dc765f4a472e7
Sha Nia Siahaan <shaniashn76@gmail.com>
2023-08-18 09:46:26 +0700

    Initial Commit

---
+++ ElectraMC1/ElectraMC1App.swift
@@ -0,0 +1,17 @@
+//
+//  ElectraMC1App.swift
+//  ElectraMC1
+//
+//  Created by Sha Nia Siahaan on 18/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ElectraMC1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
